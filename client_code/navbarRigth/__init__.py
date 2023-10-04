e7472a9c31231411d0e220d60916c518bc6d3ca5
mariana.oliveira@cappta.com.br <mariana.oliveira@cappta.com.br>
2023-10-04 14:05:45 +0000

    Created navbarRigth.resetPassword; Edited login.resetPassword and navbarRigth

--- client_code/navbarRigth/__init__.py
+++ client_code/navbarRigth/__init__.py
@@ -5,12 +5,19 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from anvil_extras import 
+from anvil_extras import popover
+from ..login.resetPassword import resetPassword
 
 class navbarRigth(navbarRigthTemplate):
   def __init__(self, **properties):
     user = anvil.users.get_user()
     self.button_1.text = user['email']
+    column = ColumnPanel()
+    resetPasswordButton = Button(align='center', text='Redefinir Senha')
+    
+    self.button_1.popover 
     
     self.init_components(**properties)
 
+  def openResetPassword_click(self, **event_args):
+    alert('reset')

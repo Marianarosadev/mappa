a10f5f3d5112e0dc7c36231c19832a38657e4e41
mariana.oliveira@cappta.com.br <mariana.oliveira@cappta.com.br>
2023-10-10 19:58:14 +0000

    Edited ServerTpvQuery and tpvQuery

--- client_code/tpvQuery/__init__.py
+++ client_code/tpvQuery/__init__.py
@@ -5,6 +5,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
+import anvil.http
 
 class tpvQuery(tpvQueryTemplate):
   def __init__(self, **properties):
@@ -14,5 +15,24 @@ class tpvQuery(tpvQueryTemplate):
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
-    pass
+    if self.date_picker_1.date is not None:
+      url = 'https://api-datascience.cappta.com.br/transactions_by_docid_date_v1v2noxus'
+      try:
+        response = anvil.http.request( 
+          url=url,
+          method='GET',
+        )
+      except Exception as e:
+        print('e: ', e)
+    
+      # anvil.server.call(
+      #   'readTpv',
+      #   self.date_picker_1.date,  
+      #   self.date_picker_2.date, 
+      #   self.text_box_1.text,
+      #   self.text_box_2.text, 
+      # )
+    else:
+      self.label_5.text = 'Campo obrigatório'
+      self.label_5.visible = True
 
--- server_code/ServerTpvQuery.py
+++ server_code/ServerTpvQuery.py
@@ -7,6 +7,25 @@ import anvil.server
 import anvil.http
 
 @anvil.server.callable
-def readTpv(partnerDocumen, EcDocument, initialDate, finalDate):
-  anvil.http.request()
+def readTpv(initial_datetime, final_datetime=None, partner_document='', merchant_document=''):
+  url = 'https://api-datascience.cappta.com.br/transactions_by_docid_date_v1v2noxus?initial_datetime={}'.format(initial_datetime.strftime('%d/%m/%Y %H:%M:%S'))
   
+  if final_datetime is not None:
+    url+= '&final_datetime={}'.format(final_datetime.strftime('%d/%m/%Y %H:%M:%S'))  
+  if partner_document != '':
+    url+= '&partner_document={}'.format(partner_document)
+  if merchant_document != '':
+    url+= '&merchant_document={}'.format(merchant_document) 
+
+  url = 'https://api-datascience.cappta.com.br/transactions_by_docid_date_v1v2noxus'
+
+  try:
+    response = anvil.http.request( 
+      url=url,
+      method='GET',
+      json=True,
+    )
+  except Exception as e:
+    print('e: ', e)
+
+  return response
